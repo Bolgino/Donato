8573846cb674dfcb89c875ab96407f9bbe304387
Bolgino <marcobolge@gmail.com>
2026-02-25 17:26:29 +0100

    Update Sidebar.tsx

--- src/components/admin/Sidebar.tsx
+++ src/components/admin/Sidebar.tsx
@@ -13,7 +13,6 @@ interface SidebarProps {
     pending: number;
     confermati: number;
     pensarci: number;
-    rifiutati: number; // MANCAVA QUESTO!
     archivio: number;
   };
   isMobileOpen: boolean;
@@ -28,7 +27,6 @@ export default function Sidebar({ vistaAttiva, setVistaAttivo, emailAmministrato
     { nome: "Pending", badge: conteggi.pending, icona: "M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" },
     { nome: "Visite Confermate", badge: conteggi.confermati, icona: "M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" },
     { nome: "Ci voglio pensare", badge: conteggi.pensarci, icona: "M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" },
-    { nome: "Rifiutati (No)", badge: conteggi.rifiutati, icona: "M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" },
     { nome: "Archivio", badge: conteggi.archivio, icona: "M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" },
     { nome: "Rubrica Prof", icona: "M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" }
   ];
