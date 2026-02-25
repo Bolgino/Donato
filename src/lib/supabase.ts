2ae829b2b832d6d346cc0342f4a6e2b0d121bc48
Bolgino <marcobolge@gmail.com>
2026-02-25 19:13:32 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    // Usa sessionStorage se siamo nel browser, altrimenti undefined
+    storage: typeof window !== 'undefined' ? window.sessionStorage : undefined,
+    persistSession: true, // Ora persisterà solo finché la scheda è aperta
+  },
+})
